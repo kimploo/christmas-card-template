c6c025ac1a43c0bb75fd4ad38e4f7105b9c771c8
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-02 02:36:22 +0900

    CCT-2 #comment chores: use tsconfig-path #time 15m

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react(), basicSsl(), tsconfigPaths()],
 });
