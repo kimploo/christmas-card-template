658538b6e883fc91e4ecb32f1b53bb714b7816f0
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-14 22:42:10 +0900

    CCT-3 #time 30m Preview 제작중

---
+++ src/components/PreviewInputContainer copy.tsx
@@ -0,0 +1,69 @@
+import { Box, Textarea, TextInput } from '@mantine/core';
+import { useState } from 'react';
+
+export const PreviewInputContainer = () => {
+  const [inputTo, setInputTo] = useState('To. ');
+  const [inputFrom, setInputFrom] = useState('From. ');
+
+  const handleTo: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputTo(event.target.value);
+    return;
+  };
+  const handleFrom: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputFrom(event.target.value);
+    return;
+  };
+
+  return (
+    <>
+      <Box
+        sx={(theme) => ({
+          textAlign: 'center',
+          maxWidth: `${theme.breakpoints.sm - 16 * 8}px`,
+          width: `${window.innerWidth - 16 * 4}px`,
+        })}
+      >
+        <TextInput
+          size={'lg'}
+          sx={() => ({
+            backgroundColor: '#F6F3A5',
+            borderBottom: '1px solid #ced4da',
+          })}
+          variant={'unstyled'}
+          placeholder="To. "
+          value={inputTo}
+          onChange={handleTo}
+          disabled
+        ></TextInput>
+        <Textarea
+          size={'lg'}
+          sx={{
+            backgroundColor: '#F6F3A5',
+            borderBottom: '1px solid #ced4da',
+          }}
+          variant={'unstyled'}
+          placeholder={`효디 작가의 모바일 축하 카드 생성기에 오신 것을 환영합니다. 여러분이 원하는 메시지를 담은 모바일 축하 카드를 적어보세요.
+`}
+          autosize
+          disabled
+        ></Textarea>
+        <TextInput
+          size={'lg'}
+          sx={{
+            fontColor: '#3E3A39',
+            backgroundColor: '#F6F3A5',
+            borderBottom: '1px solid #ced4da',
+          }}
+          variant={'unstyled'}
+          placeholder="From. "
+          value={inputFrom}
+          onChange={handleFrom}
+          disabled
+        ></TextInput>
+        {/* To. From. 남겨두기 */}
+        {/* 폰트 컬러 바꾸기 color: '#3E3A39', */}
+        {/* placeholder 중앙정렬 */}
+      </Box>
+    </>
+  );
+};
--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -1,3 +1,31 @@
+import { Footer } from '@components/Footer';
+import { MainArtwork } from '@components/MainArtwork';
+import { PreviewInputContainer } from '@components/PreviewInputContainer copy';
+import { Box, Button, Flex } from '@mantine/core';
+
 export const Preview = () => {
-  return <></>;
+  return (
+    <>
+      <Flex bg={`linear-gradient(180deg, #E6E6E6 80%, #F6F3A5 20%)`} justify={'center'}>
+        <MainArtwork src={'https://team-hh.s3.ap-northeast-2.amazonaws.com/christmas-card-template/2022-hyodee-christmas-card.png'}></MainArtwork>
+      </Flex>
+      <Flex bg={`#F6F3A5`} justify={'center'} pt={'1rem'}>
+        {/* 기기에 따라서 viewport 너비에 맞게 input의 너비가 조정이 되어야 한다. 현재는 모바일 전용 */}
+        <Box
+          sx={(theme) => ({
+            textAlign: 'center',
+            maxWidth: `${theme.breakpoints.sm - 16 * 8}px`,
+            width: `${window.innerWidth - 16 * 4}px`,
+          })}
+        >
+          <PreviewInputContainer></PreviewInputContainer>
+          <Flex justify={'center'} direction={'column'} gap={'1rem'}>
+            <Button>공유하기</Button>
+            <Button>이전에 작성한 카드 확인하기</Button>
+          </Flex>
+          <Footer></Footer>
+        </Box>
+      </Flex>
+    </>
+  );
 };
