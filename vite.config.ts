9c601b53be193f5896429aff35fa66d25182a3a8
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-14 22:42:27 +0900

    feat: 카로셀 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl(), tsconfigPaths()],
+  plugins: [
+    react(),
+    basicSsl(),
+    tsconfigPaths(),
+    // avoid circular reference https://github.com/vitejs/vite/issues/3033
+    {
+      name: 'singleHMR',
+      handleHotUpdate({ modules }) {
+        modules.map((m) => {
+          m.importedModules = new Set();
+          m.importers = new Set();
+        });
+
+        return modules;
+      },
+    },
+  ],
 });
