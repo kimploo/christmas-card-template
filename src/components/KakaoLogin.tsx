1de9899ecbd2ec5dc710f2acf8b4fd3dd38c4aff
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-17 03:30:29 +0900

    chore: remove unused code

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,34 +1,17 @@
 import { Center, Box, Image, Button } from '@mantine/core';
-import axios from 'axios';
-import { useContext, useState } from 'react';
-import { KakaoLoginContext } from 'src/main';
 
 export const KakaoLogin = () => {
   const { VITE_SERVER_URI, MODE, DEV, PROD } = import.meta.env;
-  const [kakaoToken, setToken] = useState('');
-
-  const loginInfo = useContext(KakaoLoginContext);
-
-  const getCookie = (name: string) => {
-    const parts = document.cookie.split(name + '=');
-    if (parts.length === 2) {
-      return parts[1].split(';')[0];
-    }
-    return '';
-  };
-
   const redirectUri = DEV ? 'http://localhost:3000/auth' : PROD ? VITE_SERVER_URI + '/auth' : '';
   console.log(redirectUri);
 
   const handleLogin = () => {
-    // axios.get(redirectUri);
     Kakao.Auth.authorize({
       redirectUri,
     });
   };
 
   const displayToken = () => {
-    console.log('here');
     Kakao.Auth.getStatusInfo((res) => {
       console.log(res);
     });
