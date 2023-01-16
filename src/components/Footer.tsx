217ac7b554760dd38f16b81f9d90fcbf31e7eaaf
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-17 03:28:09 +0900

    chore: layout feedback applied

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Anchor, Text } from '@mantine/core';
 export const Footer = () => {
   return (
     <>
-      <Text color={'#fbffb0'} pb={'1rem'}>
+      <Text color={'#fbffb0'} pt={'1rem'} pb={'3rem'}>
         Copyright {new Date().getFullYear()}.{' '}
         <Anchor underline={true} href="https://www.instagram.com/hyodee.r/">
           Hyodee
