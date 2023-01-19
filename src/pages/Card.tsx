5fed759a54b53f4e1853b437ef5eac5602ecb007
Hong Shik Branden Kim <kimploo@gmail.com>
2023-01-20 00:23:56 +0900

    fix: snowfall

--- src/pages/Card.tsx
+++ src/pages/Card.tsx
@@ -1,34 +1,69 @@
-import { CardInputContainer } from '@components/CardInputContainer';
-import { MainArtwork } from '@components/MainArtwork';
+import { useContext, useEffect, useState } from 'react';
 import { Footer } from '@components/Footer';
-import { Carousel } from '@mantine/carousel';
-import { Flex, Image, Box, Button } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { MainArtwork } from '@components/MainArtwork';
 import { PreviewInputContainer } from '@components/PreviewInputContainer';
+import { Anchor, Box, Button, Flex, Text } from '@mantine/core';
+import { ShareModal } from '@components/ShareModal';
 import axios from 'axios';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { LoginContext } from 'src/App';
+import { urlMaker } from 'src/util/urlMaker';
+import Snowfall from 'react-snowfall';
+import { SnowfallContainer } from '@components/SnowfallContainer';
 
-interface CardData {
-  from: string;
+const { DEV, PROD, VITE_CLIENT_DOMAIN_DEV, VITE_CLIENT_DOMAIN_PROD } = import.meta.env;
+
+interface CardInfo {
+  artwork: string;
   to: string;
   msg: string;
-  userId: number;
+  from: string;
 }
 
 export const Card = () => {
-  const [card, setCard] = useState<CardData | null>(null);
+  const domain = PROD ? VITE_CLIENT_DOMAIN_PROD : VITE_CLIENT_DOMAIN_DEV;
+  const { loginState } = useContext(LoginContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [cardInfo, setCardInfo] = useState<CardInfo>({
+    artwork: '',
+    to: '',
+    msg: '',
+    from: '',
+  });
+  const { cardId } = useParams();
+
+  const getCard = () => {
+    axios
+      .get(urlMaker(`card/${cardId}`).href, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setCardInfo(res.data);
+        setIsLoading(false);
+        console.log('data?', res.data);
+      });
+  };
 
   useEffect(() => {
-    axios.get('http://localhost:3000/card/1', { withCredentials: true }).then((res) => {
-      setCard(res.data);
-    });
+    if (PROD) {
+      getCard();
+    } else {
+      setCardInfo({
+        artwork: 'Asset-100-1.png',
+        to: 'To. 테스트입니다.',
+        msg: '테스트 메시지 입니다.',
+        from: 'From. 테스트입니다.',
+      });
+      setIsLoading(false);
+    }
   }, []);
 
   return (
     <>
-      <Flex bg={`linear-gradient(180deg, #E6E6E6 80%, #F6F3A5 20%)`} justify={'center'}>
-        <MainArtwork src={'https://team-hh.s3.ap-northeast-2.amazonaws.com/christmas-card-template/2022-hyodee-christmas-card.png'}></MainArtwork>
+      <Flex bg={`linear-gradient(180deg, #F3F19D 80%, #FCCB6B 20%)`} justify={'center'} pt={'2rem'} pb={'1rem'}>
+        {isLoading ? null : <MainArtwork src={domain + '/' + cardInfo.artwork}></MainArtwork>}
       </Flex>
-      <Flex bg={`#F6F3A5`} justify={'center'} pt={'1rem'}>
+      <Flex bg={`#FCCB6B`} justify={'center'}>
         {/* 기기에 따라서 viewport 너비에 맞게 input의 너비가 조정이 되어야 한다. 현재는 모바일 전용 */}
         <Box
           sx={(theme) => ({
@@ -37,13 +72,30 @@ export const Card = () => {
             width: `${window.innerWidth - 16 * 4}px`,
           })}
         >
-          {card ? <PreviewInputContainer from={card.from} to={card.to} msg={card.msg}></PreviewInputContainer> : null}
-          <Flex justify={'center'} direction={'column'} gap={'1rem'}>
-            <Button>나도 만들기</Button>
-          </Flex>
-          <Footer />
+          <>
+            {isLoading ? null : (
+              <>
+                <PreviewInputContainer to={cardInfo.to} msg={cardInfo.msg} from={cardInfo.from}></PreviewInputContainer>
+                <Anchor href="https://card.teamhh.link">
+                  <Button
+                    color={'orange'}
+                    sx={(theme) => ({
+                      maxWidth: `${(theme.breakpoints.sm - 16 * 8) / 2}px`,
+                    })}
+                    mt={'2rem'}
+                    mb={'1rem'}
+                    radius={'md'}
+                  >
+                    나도 카드 만들기
+                  </Button>
+                </Anchor>
+              </>
+            )}
+          </>
+          <Footer></Footer>
         </Box>
       </Flex>
+      <SnowfallContainer onOff={!isLoading}></SnowfallContainer>
     </>
   );
 };
